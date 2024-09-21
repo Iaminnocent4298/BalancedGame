eb76256a91c8736ac67cb861c5e739407d92c334
Colin Cai <therealcreative0708@gmail.com>
2024-09-20 22:53:19 -0400

    uhhhhh idk actually

--- src/main.py
+++ src/main.py
@@ -259,4 +259,4 @@ atexit.register(deinit)
 app.secret_key = bytes.fromhex(os.getenv("SECRET_KEY"))
 
 if __name__ == "__main__":
-    sock.run(app, host="0.0.0.0", port=3000)
+    sock.run(app, host="0.0.0.0", port=8082)
--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+set -e
+
+mkdir -p log
 cd src && python3 -m gunicorn -w 1 -b 0.0.0.0:3025 'main:app' -k gevent --worker-connections 1000 --access-logfile ../log/access.log --error-logfile ../log/general.log
